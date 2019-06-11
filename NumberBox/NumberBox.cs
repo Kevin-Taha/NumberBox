1a2c363e27f3ce3dea5db4b19f5da936ecb0def5
Kevin Taha <t-ketaha@microsoft.com>
2019-06-11 14:48:15 -0700

    Almost finished numeric validation

--- NumberBox/NumberBox.cs
+++ NumberBox/NumberBox.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * NumberBox Control Prototype
+ * @author: Kevin Taha 
+ * t-ketaha@microsoft.com
+ * 
+ * C# Prototype for UWP NumberBox (Microsoft XAML Controls Team)
+ * 
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -8,82 +18,118 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace NumberBox
 {
     public sealed partial class NumberBox : TextBox
     {
 
-        public double value { get; set; }
-        public Boolean HasError { get; set; }
+        bool HasError { get; set; }
+        double Value { get; set; }
+        Boolean BasicValidationEnabled { get; set; }
+        Boolean IsInvalidInputOverwritten { get; set; }
+
+        double MinValue { get; set; }
+        double MaxValue { get; set; }
+
+
+
 
 
         public NumberBox()
         {
+            this.HasError = false;
             this.DefaultStyleKey = typeof(TextBox);
+            this.BasicValidationEnabled = true;
+            this.IsInvalidInputOverwritten = false;
             this.LostFocus += new RoutedEventHandler(ValidateInput);
+            this.PointerExited += new PointerEventHandler(RefreshErrorState);
 
         }
 
 
 
+        public static readonly DependencyProperty HasErrorProperty =
+            DependencyProperty.Register("HasError", typeof(bool), typeof(NumberBox), new PropertyMetadata(false, HasErrorUpdated));
+
+        private static void HasErrorUpdated(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            NumberBox numBox = d as NumberBox;
+            Debug.WriteLine("5");
+            if (numBox != null)
+            {
+                if (numBox.HasError)
+                {
+                    VisualStateManager.GoToState(numBox, "Invalid", false);
+                    Debug.WriteLine("Writing Invalid State");
+                }
+                else
+                {
+                    VisualStateManager.GoToState(numBox, "Normal", false);
+                }
+            }
+        }
+
+        // Uses RegEx to validate that input is compliant
         void ValidateInput(object sender, RoutedEventArgs e)
         {
+            if ( !BasicValidationEnabled )
+            {
+                return;
+            }
+
             Regex rx = new Regex("\\d+.?\\d+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
             // Give Validaton error if no match 
             if (!rx.IsMatch(this.Text))
             {
-                this.Text = "INVALID";
+                SetErrorState(true);
 
-                // RaiseValidationError();
             }
             else
             {
-                this.Text = "VALID";
-                // UpdateValidationBox();
+                SetErrorState(false);
+
             }
         }
 
-              
-
-        private void RaiseValidationError()
+        // Sets the Error State of the TextBox. 
+        void SetErrorState(bool error)
         {
-           this.HasError = true;
+            if( error )
+            {
+                if ( this.HasError )
+                {
+                    Debug.WriteLine("2");
+                    VisualStateManager.GoToState(this, "Invalid", false);
 
-
+                }
+                else
+                {
+                    Debug.WriteLine("3");
+                    this.HasError = true;
+                }
+            }
+            else
+            {
+                Debug.WriteLine("4");
+                this.HasError = false;
+            }
         }
 
-        private void UpdateValidationBox()
+        // Ensures that invalid state persists when pointer exits, rather than resetting to normal state.
+        void RefreshErrorState(object sender, RoutedEventArgs e)
         {
-           if ( this.HasError)
-           {
-               this.HasError = false;
+            NumberBox numBox = (NumberBox) sender;
 
-               //TODO: Update Visual States
-           }
+            if ( numBox.HasError )
+            {
+                VisualStateManager.GoToState(this, "Invalid", false);
+            }
         }
 
 
-        public static readonly DependencyProperty HasErrorProperty =
-           DependencyProperty.Register("HasError", typeof(bool), typeof(NumberBox), new PropertyMetadata(false, HasErrorUpdated));
 
-        private static void HasErrorUpdated(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-           NumberBox numBox = d as NumberBox;
-
-           if ( numBox != null )
-           {
-               if ( numBox.HasError )
-               {
-                   VisualStateManager.GoToState(numBox, "InvalidState", false);
-
-               }
-               else
-               {
-                   VisualStateManager.GoToState(numBox, "ValidState", false);
-               }
-           }
-        } 
 
 
 
